187c650a1287d95a4e68449d4643b20a74788b02
Jaiden Mispy <jaiden@mispy.me>
2019-04-12 18:20:19 +1000

    Initial commit (forked from taxicab)

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import * as THREE from 'three'
+declare var require: any;
+const OrbitControls = require('three-orbit-controls')(THREE)
+import * as _ from 'lodash'
+import './index.scss'
+
+function main() {
+
+}
+main()
